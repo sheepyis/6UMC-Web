5d720cd1c3b0ca0fc63f28e4ef13121b671cc941
sheepyis <zx9086@naver.com>
2024-04-03 19:46:00 +0900

    ✨ week2_counter

--- week2/counter/src/App.jsx
+++ week2/counter/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    console.log("increase 가 클릭됨");
+    setCount(count + 1);
+  }
+
+  const decrease = () => {
+    console.log("decrease 가 클릭됨");
+    setCount(count - 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>{count}</h2>
+      <div className="buttonContainer" style={{display: "flex", gap: "1vw"}}>
+        <button onClick={increase}>+1</button>
+        <button onClick={decrease}>-1</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
