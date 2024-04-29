eff0f4cf02c28ed682d55735ed31bc56d167aa43
sheepyis <zx9086@naver.com>
2024-04-30 03:13:52 +0900

    💄 week3_movie pageContainer

--- week3/movie/src/pages/UpComingPage.jsx
+++ week3/movie/src/pages/UpComingPage.jsx
@@ -1,19 +1,11 @@
 import ListMovie from "../components/list/list-movie";
-import styled from "styled-components";
-
-const UpComingContainer = styled.div`
-    width: 100%;
-    min-height: calc(100vh - 6vw);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+import PageContainer from "../styles/PageStyle";
 
 const UpComingPage = () => {
     return (
-        <UpComingContainer>
+        <PageContainer>
             <ListMovie Url="https://api.themoviedb.org/3/movie/upcoming"/>
-        </UpComingContainer>
+        </PageContainer>
     )
 }
 
