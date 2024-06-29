a4547ec5365ec7e0b9cce17684438275f611f525
sheepyis <zx9086@naver.com>
2024-06-29 23:18:13 +0900

    🚑 week10_share KakaoShareImg

--- week10/share/src/api/ShareKakao.jsx
+++ week10/share/src/api/ShareKakao.jsx
@@ -34,7 +34,7 @@ const ShareKakao = () => {
         content: {
             title: '영화 검색 사이트', // 카카오톡 제목
             description: '#UMC #6기 #한성대 #Web #10주차',  // 카카오톡 상세 설명
-            imageUrl:'https://pbs.twimg.com/profile_images/1212031261297930241/p6kIo01N_400x400.jpg',    // 카카오톡 이미지
+            imageUrl:'https://jsh99.github.io/static/072202b54fbdab5fe9521ebb5c92f32a/6f3f2/UMC.png',    // 카카오톡 이미지
             link: {
             mobileWebUrl: 'https://week10-share.netlify.app/', // 배포 링크
             webUrl: 'https://week10-share.netlify.app/',   // 배포 링크
