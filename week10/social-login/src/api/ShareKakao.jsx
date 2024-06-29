c9f455fa83402efb815de2844043c72f97199af0
sheepyis <zx9086@naver.com>
2024-06-29 23:34:46 +0900

    👽week10_social-login shareLink

--- week10/social-login/src/api/ShareKakao.jsx
+++ week10/social-login/src/api/ShareKakao.jsx
@@ -36,16 +36,16 @@ const ShareKakao = () => {
             description: '#UMC #6기 #한성대 #Web #10주차',  // 카카오톡 상세 설명
             imageUrl:'https://pbs.twimg.com/profile_images/1212031261297930241/p6kIo01N_400x400.jpg',    // 카카오톡 이미지
             link: {
-            mobileWebUrl: 'https://week10-share.netlify.app/', // 배포 링크
-            webUrl: 'https://week10-share.netlify.app/',   // 배포 링크
+            mobileWebUrl: 'https://week10-login.netlify.app/', // 배포 링크
+            webUrl: 'https://week10-login.netlify.app/',   // 배포 링크
             },
         },
         buttons: [
             {
             title: '영화 찾아보러 가기',    // 카카오톡 버튼
             link: {
-                mobileWebUrl: 'https://week10-share.netlify.app/', // 배포 링크
-                webUrl: 'https://week10-share.netlify.app/',   // 배포 링크
+                mobileWebUrl: 'https://week10-login.netlify.app/', // 배포 링크
+                webUrl: 'https://week10-login.netlify.app/',   // 배포 링크
             },
             },
         ],
